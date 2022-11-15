6fa9c45425b9544c4c25be5892254f3b5cd9fc1d
Matt Gleich <git@mattglei.ch>
2022-11-15 17:38:52 -0500

    feat: clear func for microdot phat
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,16 @@
 pub mod matrix;
+
+const WIDTH: usize = 45;
+const HEIGHT: usize = 7;
+
+pub struct MicrodotPhat {
+    pub buffer: [[u8; WIDTH]; HEIGHT],
+    pub decimal_buffer: [u8; 6],
+}
+
+impl MicrodotPhat {
+    pub fn clear(&mut self) {
+        self.buffer = [[0; WIDTH]; HEIGHT];
+        self.decimal_buffer = [0; 6];
+    }
+}
